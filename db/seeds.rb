b36f61bd0a614b2e494eb21f4285bffb32210134
EC2 Default User <ec2-user@ip-172-31-17-124.us-east-2.compute.internal>
2018-01-23 12:29:59 +0000

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
-User.create!(name:  "Rishabh Sairawat",
-             email: "rishabh.sairawat01@gmail.com",
+User.create!(name:  "Rishabh",
+             email: "rishabh.sairawat@payu.in",
              password:              "rishabh",
              password_confirmation: "rishabh",
-             admin: true)
-
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
